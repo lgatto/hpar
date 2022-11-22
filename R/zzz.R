0a94e5f23b62aff1cec9438f65fd4ad4932aec86
Laurent Gatto <laurent.gatto@uclouvain.be>
2022-11-22 10:19:54 +0100

    update title names (omit extension)

--- R/zzz.R
+++ R/zzz.R
@@ -1,5 +1,5 @@
 .onAttach <- function(libname, pkgname) {
-    setHparOptions(hpadata = "hpaNormalTissue")
+    ## setHparOptions(hpadata = "hpaNormalTissue")
     msg <- paste0("This is hpar version ", utils::packageVersion("hpar"),
                   ",\nbased on the Human Protein Atlas\n",
                   "  Version: ", getHpaVersion(), "\n",
